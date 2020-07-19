14cc6884c4c5e593d0f300aed799a412b5c0e3f5
il-kow@mail.ru <il-kow@mail.ru>
2020-07-19 17:23:33 +0400

    [dev]:(StringButtonController changed)

--- Assets/Scripts/UI/StringButtonController.cs
+++ Assets/Scripts/UI/StringButtonController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class StringButtonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject picker;
 
-    // Update is called once per frame
-    void Update()
+    public void changeStringForPicker(int posNumber)
     {
+        picker = GameObject.FindGameObjectWithTag("Player");
         
+        Debug.Log("change picker y position");
+        float pickerYCoord = 0f;
+        switch (posNumber)
+        {
+            case 1:
+                pickerYCoord = 4.2f;
+                break;
+            case 2:
+                pickerYCoord = 3.2f;
+                break;
+            case 3:
+                pickerYCoord = 2.2f;
+                break;
+            case 4:
+                pickerYCoord = 1.2f;
+                break;
+            case 5:
+                pickerYCoord = 0.2f;
+                break;
+        }
+        
+        picker.transform.position = new Vector2(picker.transform.position.x, pickerYCoord);
     }
 }
