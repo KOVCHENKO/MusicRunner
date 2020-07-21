a657c14c35cfea3d95e45b4206bb423530e80f70
il-kow@mail.ru <il-kow@mail.ru>
2020-07-21 23:08:36 +0400

    [master]:(multiple score multiplier by numbers)

--- Assets/Scripts/Enemy/EnemyAttacked.cs
+++ Assets/Scripts/Enemy/EnemyAttacked.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
+using Util;
 
 namespace Enemy
 {
     public class EnemyAttacked : MonoBehaviour
     {
         public GameObject explosion;
-        public GameObject score;
+        public GameObject scoreTwo;
+        public GameObject scoreFour;
+        public GameObject scoreEight;
     
         void OnTriggerEnter2D(Collider2D other)
         {
@@ -17,12 +20,30 @@ namespace Enemy
                     Instantiate(explosion, transform.position, transform.rotation);
                 }
 
-                if (score)
-                {
-                    Instantiate(score, transform.position, transform.rotation);
-                }
+                DefineEarnedScore();
 
                 Object.Destroy(this.gameObject);
+            }
+        }
+
+        private void DefineEarnedScore()
+        {
+            ScoreTimer.SetScoreMultiplier(2);
+
+            switch (ScoreTimer.GetScoreMultiplier())
+            {
+                case 2: 
+                    Instantiate(scoreTwo, transform.position, transform.rotation);
+                    break;
+                case 4:
+                    Instantiate(scoreFour, transform.position, transform.rotation);
+                    break;
+                case 8:
+                    Instantiate(scoreEight, transform.position, transform.rotation);
+                    break;
+                default:
+                    Instantiate(scoreEight, transform.position, transform.rotation);
+                    break;
             }
         }
     }
---
+++ Assets/Scripts/Util/ScoreTimer.cs
@@ -0,0 +1,17 @@
+﻿namespace Util
+{
+    public static class ScoreTimer
+    {
+        private static int _scoreMultiplier;
+        
+        public static int GetScoreMultiplier()
+        {
+            return _scoreMultiplier;
+        }
+
+        public static void SetScoreMultiplier(int multiplier)
+        {
+            _scoreMultiplier += multiplier;
+        }
+    }
+}
