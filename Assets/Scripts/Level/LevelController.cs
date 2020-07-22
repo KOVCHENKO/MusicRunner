87dc36e25dea0234569ed99b475e2e750dadaf0d
il-kow@mail.ru <il-kow@mail.ru>
2020-07-22 23:50:57 +0400

    ]

---
+++ Assets/Scripts/Level/LevelController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Level
+{
+    public class LevelController : MonoBehaviour
+    {
+        void Start()
+        {
+            // Initialize script and create notes based on tempo
+        }
+    }
+}
