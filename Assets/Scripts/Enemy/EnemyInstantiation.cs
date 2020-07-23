103c427a41d28701dfa211269a995b6df73a80eb
il-kow@mail.ru <il-kow@mail.ru>
2020-07-23 16:43:45 +0400

    [dev]:(instantiation script updated)

--- Assets/Scripts/Enemy/EnemyInstantiation.cs
+++ Assets/Scripts/Enemy/EnemyInstantiation.cs
@@ -5,18 +5,24 @@ namespace Enemy
     public class EnemyInstantiation : MonoBehaviour
     {
         public GameObject enemyNote;
-        public int width = 2;
-        public int height = 2;
+        public int width = 4;
+        public int height = 4;
         
         private void Start()
         {
+            float firstStringY = 4.3f;
+            float firsrtPositionX = 0;
+
             for (int y = 0; y < height; ++y)
             {
-                for (int x = 0; x < width; ++x)
+                for (int x = 0; x < width; x++)
                 {
-                    Instantiate(enemyNote, new Vector3(x, y, 0), Quaternion.identity);
+                    Instantiate(enemyNote, new Vector3(firsrtPositionX * 3f, firstStringY, 0), Quaternion.identity);
+                    firsrtPositionX += 1f;
                 }
-            } 
+                
+                firstStringY -= 1f;
+            }
         }
     }
 }
