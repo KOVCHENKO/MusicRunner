1953e2944ad566478a0672f414b7a5ce7140d6dc
il-kow@mail.ru <il-kow@mail.ru>
2020-08-24 19:42:24 +0400

    [assets]:(Level3 assets and level)

---
+++ Assets/Scripts/Level/LevelInstantiations/LevelThreeStructure.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.ObjectModel;
+using Enemy;
+using Level.MusicalStrings;
+using UnityEngine;
+
+namespace Level.LevelInstantiations
+{
+    public class LevelThreeStructure: EnemyInstantiation
+    {
+        public void CreateLevelElements()
+        {
+            InstantiateStringBreak(new FirstString());
+        
+            InstantiateEnemyNotes(4, new SecondString(), enemyNote4);
+            
+            InstantiateStringBreak(new FifthString());
+
+            InstantiateEnemyNotes(4, new FirstString(), enemyNote4);
+            
+            InstantiateStringBreak(new FirstString());
+        
+            InstantiateEnemyNotes(4, new SecondString(), enemyNote4);
+            
+            InstantiateStringBreak(new FifthString());
+
+            InstantiateEnemyNotes(4, new FirstString(), enemyNote4);
+            
+            InstantiateStringBreak(new FirstString());
+        
+            InstantiateEnemyNotes(4, new SecondString(), enemyNote4);
+            
+            InstantiateStringBreak(new FifthString());
+
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote4);
+            
+            IncreaseXPosition();
+            
+            InstantiateEnemyNotes(4, new ThirdString(), enemyNote8);
+            
+            InstantiateStringBreak(new FifthString());
+
+            InstantiateEnemyNotes(4, new ForthString(), enemyNote8);
+
+            InstantiateStringBreak(new FirstString());
+            
+            InstantiateEnemyNotes(4, new ThirdString(), enemyNote8);
+
+            InstantiateStringBreak(new FifthString());
+
+            InstantiateEnemyNotes(4, new ForthString(), enemyNote8);
+            
+            InstantiateStringBreak(new FirstString());
+
+            // Finish
+            InstantiateFinish();
+        }
+    }
+}
