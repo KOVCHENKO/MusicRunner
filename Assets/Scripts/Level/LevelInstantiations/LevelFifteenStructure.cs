fb68b044714b488f3b2818f497529f15bc73abc6
il-kow@mail.ru <il-kow@mail.ru>
2020-09-15 16:17:52 +0400

    [dev]:(LevelFifteenStructure updated)

--- Assets/Scripts/Level/LevelInstantiations/LevelFifteenStructure.cs
+++ Assets/Scripts/Level/LevelInstantiations/LevelFifteenStructure.cs
@@ -11,8 +11,299 @@ namespace Level.LevelInstantiations
 
         public override void CreateLevelElements()
         {
-            InstantiateEnemyNotes(4, new FirstString(), enemyNote2, 2);
-
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote4, 4);
+            InstantiateEnemyNotes(2, new SecondString(), pause4, 0);
+            InstantiateEnemyNotes(2, new ForthString(), enemyNote4, 4);
+            InstantiateEnemyNotes(2, new ForthString(), pause4, 0);
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote4, 4);
+            InstantiateEnemyNotes(2, new FirstString(), pause4, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote4, 4);
+            InstantiateEnemyNotes(2, new ThirdString(), pause4, 0);
+            InstantiateEnemyNotes(2, new FifthString(), enemyNote4, 4);
+            InstantiateEnemyNotes(2, new FifthString(), pause4, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote4, 4);
+            InstantiateEnemyNotes(2, new SecondString(), pause4, 0);
+            InstantiateEnemyNotes(2, new ForthString(), enemyNote4, 4);
+            InstantiateEnemyNotes(2, new ForthString(), pause4, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote4, 4);
+            InstantiateEnemyNotes(2, new ThirdString(), pause4, 0);
+            
+            IncreaseXPosition();
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            // Change position
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            // Another strings
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            // Another strings
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            // Another strings
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            // Another strings
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            // Another strings
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new SecondString(), pause8, 0);
+            InstantiateEnemyNotes(1, new FifthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FifthString(), pause8, 0);
+            
+            // Another strings
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(1, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(1, new ForthString(), pause8, 0);
+            
+            // Another strings by two near 
+            InstantiateEnemyNotes(2, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ForthString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ForthString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ForthString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ForthString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            // Another strings by two near 
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            
+            // Another strings by two near 
+            InstantiateEnemyNotes(2, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ForthString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ForthString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ForthString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new ForthString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ForthString(), pause8, 0);
+            InstantiateEnemyNotes(2, new ThirdString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new ThirdString(), pause8, 0);
+            
+            // Another strings by two near 
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            
+            InstantiateEnemyNotes(2, new FirstString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new FirstString(), pause8, 0);
+            InstantiateEnemyNotes(2, new SecondString(), enemyNote8, 8);
+            InstantiateEnemyNotes(2, new SecondString(), pause8, 0);
+            
+            // Finish
             InstantiateFinish();
         }
     }
