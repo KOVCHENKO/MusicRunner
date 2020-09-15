c30dab87e393427b5efadacb845704d4f96508c2
il-kow@mail.ru <il-kow@mail.ru>
2020-09-15 16:52:02 +0400

    [dev]:(LevelEighteenStructure updated)

--- Assets/Scripts/Level/LevelInstantiations/LevelEighteenStructure.cs
+++ Assets/Scripts/Level/LevelInstantiations/LevelEighteenStructure.cs
@@ -11,7 +11,65 @@ namespace Level.LevelInstantiations
 
         public override void CreateLevelElements()
         {
-            InstantiateEnemyNotes(4, new FirstString(), enemyNote2, 2);
+             InstantiateEnemyNotes(4, new FirstString(), pause2, 2);
+            InstantiateEnemyNotes(4, new SecondString(), pause2, 2);
+            InstantiateEnemyNotes(4, new ThirdString(), pause2, 2);
+            InstantiateEnemyNotes(4, new ForthString(), pause2, 2);
+            InstantiateEnemyNotes(4, new FifthString(), pause2, 2);
+            InstantiateEnemyNotes(4, new ForthString(), pause2, 2);
+            InstantiateEnemyNotes(4, new ThirdString(), pause2, 2);
+            InstantiateEnemyNotes(4, new SecondString(), pause2, 2);
+            InstantiateEnemyNotes(4, new FirstString(), pause2, 2);
+            
+            InstantiateEnemyNotes(2, new FirstString(), pause2, 2);
+            InstantiateEnemyNotes(2, new SecondString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ThirdString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ForthString(), pause2, 2);
+            InstantiateEnemyNotes(2, new FifthString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ForthString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ThirdString(), pause2, 2);
+            InstantiateEnemyNotes(2, new SecondString(), pause2, 2);
+            InstantiateEnemyNotes(2, new FirstString(), pause2, 2);
+            
+            InstantiateEnemyNotes(1, new FirstString(), pause2, 2);
+            InstantiateEnemyNotes(1, new SecondString(), pause2, 2);
+            InstantiateEnemyNotes(1, new ThirdString(), pause2, 2);
+            InstantiateEnemyNotes(1, new ForthString(), pause2, 2);
+            InstantiateEnemyNotes(1, new FifthString(), pause2, 2);
+            InstantiateEnemyNotes(1, new ForthString(), pause2, 2);
+            InstantiateEnemyNotes(1, new ThirdString(), pause2, 2);
+            InstantiateEnemyNotes(1, new SecondString(), pause2, 2);
+            InstantiateEnemyNotes(1, new FirstString(), pause2, 2);
+            InstantiateEnemyNotes(1, new FirstString(), pause2, 2);
+            
+            InstantiateEnemyNotes(1, new FirstString(), enemyNote2, 2);
+
+            InstantiateEnemyNotes(2, new FirstString(), pause2, 2);
+            InstantiateEnemyNotes(2, new SecondString(), pause2, 2);
+            InstantiateEnemyNotes(2, new FirstString(), pause2, 2);
+            InstantiateEnemyNotes(2, new SecondString(), pause2, 2);
+            InstantiateEnemyNotes(2, new FirstString(), pause2, 2);
+            InstantiateEnemyNotes(2, new SecondString(), pause2, 2);
+            
+            InstantiateEnemyNotes(2, new ThirdString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ForthString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ThirdString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ForthString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ThirdString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ForthString(), pause2, 2);
+            
+            InstantiateEnemyNotes(2, new FifthString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ForthString(), pause2, 2);
+            InstantiateEnemyNotes(2, new FifthString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ForthString(), pause2, 2);
+            InstantiateEnemyNotes(2, new FifthString(), pause2, 2);
+            InstantiateEnemyNotes(2, new ForthString(), pause2, 2);
+            
+            IncreaseXPosition();
+            IncreaseXPosition();
+            
+            // Finish
+            InstantiateFinish();
 
             InstantiateFinish();
         }
